2e26326a472a27311ad763b3c3bf28e554c6bbb0
Sneha Talwalkar <81658143+snehatalwalkar@users.noreply.github.com>
2023-03-08 02:03:58 -0500

    Updating script
    download specified

--- download.sh
+++ download.sh
@@ -1,9 +1,9 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the GNU General Public License version 3.
 
-PRESIGNED_URL=""             # replace with presigned url from email
-MODEL_SIZE="7B,13B,30B,65B"  # edit this list with the model sizes you wish to download
-TARGET_FOLDER=""             # where all files should end up
+PRESIGNED_URL="https://dobf1k6cxlizq.cloudfront.net/*?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9kb2JmMWs2Y3hsaXpxLmNsb3VkZnJvbnQubmV0LyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE2NzgzNzA0NjF9fX1dfQ__&Signature=JHpOn6eDRudPBC01sKTZu~ZOExwzpYXDGTNdGkvPLrnNHtjHig6BTR9N5gc~RQCs65bCmyTVKN~3Ma8rixu5Yem0nrhq80sb58-BLI-PHRQYwN~zhTRt6lhXg4z94mmWQfedH~23TfHuXaJmUy2qgG5~S6ZXM7ianTrxw2EFVt~tPE8DFXYII7mL29oxaxTPrWgKryXXsmxyn5PBhvsKAtvx9VCsWS3Vm6PAxo4-BQ1qG3-LQ3z24Av6iaHXHtAzKfYsN~rGU~FkDhL9vbmoe4~lBhPv1WsnmWk5NDTmAHWIwErVp6D0KWI8YKXjoUOzZONFLru0PSM8XCQXjz1wBA__&Key-Pair-Id=K231VYXPC1TA1R"             # replace with presigned url from email
+MODEL_SIZE="7B"  # edit this list with the model sizes you wish to download
+TARGET_FOLDER="Downloads"             # where all files should end up
 
 declare -A N_SHARD_DICT
 
